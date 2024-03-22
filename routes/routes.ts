ef53ea38ab2c807c5052f2926bee8c96e98590f0
joaoagr1 <joao.rollo1@gmail.com>
2024-03-21 23:20:09 -0300

    :recycle: refactor: Organizando arquitetura para escalar aplicação com outras rotas

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import userController from './user-controller'
+import userController from '../controller/user-controller'
 
 const routes = Router()
 routes.post('/user', userController.create)
