a27cef9f2070ee842ae316b028cda331e524831b
joaoagr1 <joao.rollo1@gmail.com>
2024-04-12 18:59:13 -0300

    syncando com repositorio pull da materia

--- services/category-service.ts
+++ services/category-service.ts
@@ -1,6 +1,6 @@
 import CategoryInterface, { Category } from "../entity/category";
-import categoryModel from "../entity/category";
-
+import Userinterface from "../entity/user"
+import taskModel from "../entity/task"
 export default class CategoryService {
     async create(category: CategoryInterface) {
         const createdCategory = await Category.create(category);
@@ -10,6 +10,24 @@ export default class CategoryService {
     async findById(id: CategoryInterface["_id"]) {
         const findedCategory = await Category.findById(id);
         return findedCategory;
+    }
+
+    async findAllCategoryByUser(id: Userinterface["_id"]){
+        var categoryUserList:any = [];
+        const distinctCategories = await taskModel.distinct('category', { responsibleUser: id });
+        distinctCategories.forEach(async(index) => {
+           // console.log(index)
+        const findedCategory = await Category.findById(index);
+            //console.log(findedCategory)
+        categoryUserList.push(findedCategory)
+            console.log(categoryUserList)
+
+        })
+
+    return categoryUserList;
+
+   
+
     }
 }
 
